a5d4a4d0863621d866dcf49cabf40db416838f2f
nicolasmss <70489819+nicolasmss@users.noreply.github.com>
2021-08-30 18:34:21 -0300

    Initial commit

---
+++ CtrlCorridasControler.java
@@ -0,0 +1,79 @@
+package com.bcopstein.CtrlCorredorV1;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/ctrlCorridas")
+public class CtrlCorridasControler {
+    private JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public CtrlCorridasControler(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+
+        this.jdbcTemplate.execute("DROP TABLE corredores IF EXISTS");
+        this.jdbcTemplate.execute("CREATE TABLE corredores("
+                + "cpf VARCHAR(255), nome VARCHAR(255), genero VARCHAR(255), diaDn int, mesDn int, anoDn int, PRIMARY KEY(cpf))");
+
+        this.jdbcTemplate.batchUpdate(
+                "INSERT INTO corredores(cpf,nome,genero,diaDn,mesDn,anoDn) VALUES ('10001287','Luiz','masculino',22,5,1987)");
+
+        this.jdbcTemplate.execute("DROP TABLE eventos IF EXISTS");
+        this.jdbcTemplate.execute("CREATE TABLE eventos("
+                + "id int, nome VARCHAR(255), dia int, mes int, ano int, distancia int, horas int, minutos int, segundos int,PRIMARY KEY(id))");
+
+/*        this.jdbcTemplate.batchUpdate(
+                "INSERT INTO eventos(id,nome,dia,mes,ano,distancia,horas,minutos,segundos) VALUES" +
+                " ('1','Poa Day Run',22,5,2019,5,0,35,32)");*/
+    }
+
+    @GetMapping("/corredor")
+    @CrossOrigin(origins = "*")
+    public List<Corredor> consultaCorredor() {
+        List<Corredor> resp = this.jdbcTemplate.query("SELECT * from corredores",
+                (rs, rowNum) -> new Corredor(rs.getString("cpf"), rs.getString("nome"), rs.getInt("diaDn"),
+                        rs.getInt("mesDn"), rs.getInt("anoDn"), rs.getString("genero")));
+        return resp;
+    }
+
+    @PostMapping("/corredor")
+    @CrossOrigin(origins = "*")
+    public boolean cadastraCorredor(@RequestBody final Corredor corredor) {
+        // Limpa a base de dados
+        this.jdbcTemplate.batchUpdate("DELETE from Corredores");
+        // Então cadastra o novo "corredor único"
+        this.jdbcTemplate.update("INSERT INTO corredores(cpf,nome,diaDn,mesDn,anoDn,genero) VALUES (?,?,?,?,?,?)",
+                corredor.getCpf(), corredor.getNome(), corredor.getDiaDn(), corredor.getMesDn(), corredor.getAnoDn(),
+                corredor.getGenero());
+        return true;
+    }
+
+    @GetMapping("/eventos")
+    @CrossOrigin(origins = "*")
+    public List<Evento> consultaEventos() {
+        List<Evento> resp = this.jdbcTemplate.query("SELECT * from eventos",
+                (rs, rowNum) -> new Evento(rs.getInt("id"), rs.getString("nome"), rs.getInt("dia"), rs.getInt("mes"),
+                        rs.getInt("ano"), rs.getInt("distancia"), rs.getInt("horas"), rs.getInt("minutos"),
+                        rs.getInt("segundos")));
+        return resp;
+    }
+
+    @PostMapping("/eventos") // adiciona evento no único corredor
+    @CrossOrigin(origins = "*")
+    public boolean informaEvento(@RequestBody final Evento evento) {
+        this.jdbcTemplate.update(
+                "INSERT INTO eventos(id,nome,dia,mes,ano,distancia,horas,minutos,segundos) VALUES (?,?,?,?,?,?,?,?,?)",
+                evento.getId(), evento.getNome(), evento.getDia(), evento.getMes(), evento.getAno(),
+                evento.getDistancia(), evento.getHoras(), evento.getMinutos(), evento.getSegundos());
+        return true;
+    }
+}
